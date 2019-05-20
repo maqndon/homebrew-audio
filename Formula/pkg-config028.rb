9b794a4fdb9b40c8807f04bcfb8b0b7886451f87
Marcel Caraballo <maqndon@users.noreply.github.com>
2019-05-20 19:25:54 +0200

    Update pkg-config028.rb

--- Formula/pkg-config028.rb
+++ Formula/pkg-config028.rb
@@ -2,7 +2,7 @@ class PkgConfig028 < Formula
   desc "Manage compile and link flags for libraries"
   homepage "https://freedesktop.org/wiki/Software/pkg-config/"
   url "http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz"
-  sha256 "D3611D825DE06EA099EF8BD422B28623299A7FE302A8E39AD10C0DD80CD1D622"
+  sha256 "6b6eb31c6ec4421174578652c7e141fdaae2dabad1021f420d8713206ac1f845"
 
   def install
     pc_path = %W[
