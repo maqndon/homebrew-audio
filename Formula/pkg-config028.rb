bdf2dcb7ab8ccd4f4c26c9a870965895bde0517f
Marcel Caraballo <maqndon@users.noreply.github.com>
2019-05-20 19:10:24 +0200

    Update pkg-config028.rb

--- Formula/pkg-config028.rb
+++ Formula/pkg-config028.rb
@@ -1,1 +1,22 @@
+class pkg-config < Formula
+  desc "pkg-config"
+  homepage "https://www.freedesktop.org/wiki/Software/pkg-config/"
+  url "http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz"
+  sha256 "d3611d825de06ea099ef8bd422b28623299a7fe302a8e39ad10c0dd80cd1d622"
 
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
