e2888f743e5dbba45e2fe33cc2c1767c3f96ea97
Marcel Caraballo <maqndon@users.noreply.github.com>
2019-05-25 08:56:19 +0200

    Update libtool.rb

--- Formula/libtool.rb
+++ Formula/libtool.rb
@@ -2,7 +2,7 @@ class Libtool < Formula
   desc "Generic library support script"
   homepage "https://www.gnu.org/software/libtool/"
   url "https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz"
-  mirror "https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.xz"
+  mirror "http://ftpmirror.gnu.org/libtool/libtool-2.4.2.tar.gz"
   sha256 "7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f"
   revision 1
 
